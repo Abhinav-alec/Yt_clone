b3e84951908678ca1445b1e725767d755171bf65
Abhinav kumar jha <abhinav234765@gmail.com>
2025-07-20 10:12:50 +0530

    SearchFeed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { Box, CircularProgress, Stack } from "@mui/material";
 
-const Loader = () => {
-  return <div>This is Loader</div>;
-};
+const Loader = () => (
+  <Box minHeight="95vh">
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      height="80vh"
+    >
+      <CircularProgress />
+    </Stack>
+  </Box>
+);
+
 export default Loader;
